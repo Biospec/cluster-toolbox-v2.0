44cab12c12f6a62d3640cd40f8860cb81c7a8420
Laboratory for Bioanalytical Spectroscopy <Yun.Xu@Liverpool.ac.uk>
2020-05-27 16:45:25 +0100

    Updated normalization routine in normtot

--- cluster_toolbox/normtot.m
+++ cluster_toolbox/normtot.m
@@ -4,8 +4,12 @@ function [normal_tot] = normtot(M)
 %takes matrix M and makes normal_tot
 %
 % Copyright (c) 1996, Royston Goodacre
-%
+% Updated 2020, Yun Xu
 
-rowsum=sum(M');
-normal_tot=(M'*inv(diag(rowsum)))';
+% rowsum=sum(M');
+% normal_tot=(M'*inv(diag(rowsum)))';
+
+% New code
+rowsum = sum(M, 2);
+normal_tot = M ./ repmat(rowsum, 1, size(M, 2));
 
