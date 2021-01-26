4023d7fc658847c9eaee6ebfd8a41eadd7f5b3e7
Laboratory for Bioanalytical Spectroscopy <Yun.Xu@Liverpool.ac.uk>
2021-01-26 21:13:27 +0000

    gradientclass_plot2 bug fix

--- cluster_toolbox/gradientclass_plot2.m
+++ cluster_toolbox/gradientclass_plot2.m
@@ -7,15 +7,53 @@ function gradientclass_plot2(u, label1, label2)
 
 
 if iscell(label1)
-    unique_color=unique(label1);
+    unique_label1 = unique(label1); 
+    num_flag = true;
+    for i = 1:numel(unique_label1)
+        if isempty(str2num(unique_label1{i}))
+            num_flag = false;
+        end
+    end
+    if ~num_flag
+        unique_color=unique(label1);        
+    else        
+        for i = 1:size(label1, 1)
+            label1_tmp(i,1) = str2num(label1{i});
+        end
+        unique_label1 = unique(label1_tmp);
+        for i = 1:numel(unique_label1)
+            unique_color{i} = num2str(unique_label1(i));
+        end
+        clear label1_tmp
+    end
     no_colors=length(unique_color);
 elseif ischar(label1)
-    unique_color=cellstr(unique(label1,'rows'));
-    no_colors=length(unique_color);
-    label1=cellstr(label1);
+    num_flag = true;
+    unique_label1 = unique(label1, 'rows');
+    for i = 1:size(unique_label1, 1)
+        if isempty(str2num(unique_label1(i, :)))
+            num_flag = false;
+        end
+    end
+    if ~num_flag
+        unique_color = cellstr(unique(label1,'rows'));
+        no_colors = length(unique_color);
+        label1 = cellstr(label1);
+    else
+        for i = 1:size(label1, 1)
+            label1_tmp(i, 1) = str2num(label1(i, :));
+        end
+        unique_label1 = unique(label1_tmp);
+        for i = 1:numel(unique_label1)
+            unique_color{i} = num2str(unique_label1(i));
+        end
+        no_colors = length(unique_color);
+        label1 = cellstr(label1);
+    end
 else
     unique_color=cellstr(num2str(unique(label1)));
     no_colors=length(unique_color);
+    label1 = cellstr(num2str(label1));
 end
 
 if isnumeric(label2)
@@ -26,14 +64,14 @@ end
 
 color_map=jet(no_colors);
 
-symbol=['o';'x';'+';'*';'s';'d';'v';'^';'<';'>';'p';'h';'.'];
+symbol=['o';'s';'d';'v';'^';'<';'>';'p';'h';'x';'+';'*';'.'];
 
 figure
 hold on
 count=1;
 for i=1:no_colors
-    label2_now=label2(ismember(label1, unique_color{i}));
-    symbols_now=unique(label2_now);
+    label1_now=label1(ismember(label1, unique_color{i}));
+    symbols_now=unique(label2);
     no_symbols=length(symbols_now);
     for ii=1:no_symbols
         if ii>length(symbol)
@@ -43,12 +81,16 @@ for i=1:no_colors
         end
         sample_idx=find(ismember(label1, unique_color{i}) & ismember(label2, symbols_now{ii}));
         h=plot(u(sample_idx,1), u(sample_idx,2), symbol(symbol_idx));
-        set(h,'color', color_map(i,:))
-        label_legend{count}=[unique_color{i} ' ' symbols_now{ii}];
-        count=count+1;
+        set(h,'markersize', 8, 'color', color_map(i,:), 'markerfacecolor', ...
+            color_map(i, :))
+        if i == 1
+            label_legend{count}=symbols_now{ii};
+            count=count+1;
+        end
     end
 end
-
+h = colorbar;
+set(h, 'Ytick', linspace(0, 1, length(unique_color)), 'YtickLabel', unique_color);
 legend(label_legend);
 % h=xlabel('Discriminant function 1'); set(h,'fontsize',14)
 % h=ylabel('Discriminant function 2'); set(h,'fontisze',14)
