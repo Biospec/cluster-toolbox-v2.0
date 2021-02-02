21d1d8088ec52b9ce6c4fc8f3169c2df37a25bba
Laboratory for Bioanalytical Spectroscopy <Yun.Xu@Liverpool.ac.uk>
2021-02-02 16:56:48 +0000

    gradientclass_plot2 updated
    3-D plot added and colormap changed to parula.

--- cluster_toolbox/gradientclass_plot2.m
+++ cluster_toolbox/gradientclass_plot2.m
@@ -1,7 +1,7 @@
 function gradientclass_plot2(u, label1, label2)
 
 %gradientclass_plot(u, label1, label2)
-%u: the data matrix 
+%u: the data matrix with no more than 3 columns. 
 %label1: the class label which shall be separated by colour gradient
 %label2: the class label which shall be separated by different symbols
 
@@ -62,38 +62,73 @@ elseif ischar(label2)
     label2=cellstr(label2);
 end
 
-color_map=jet(no_colors);
+color_map=parula(no_colors);
 
 symbol=['o';'s';'d';'v';'^';'<';'>';'p';'h';'x';'+';'*';'.'];
 
-figure
-hold on
-set(gcf, 'colormap', jet(256));
-count=1;
-for i=1:no_colors
-    label1_now=label1(ismember(label1, unique_color{i}));
-    symbols_now=unique(label2);
-    no_symbols=length(symbols_now);
-    for ii=1:no_symbols
-        if ii>length(symbol)
-            symbol_idx=rem(ii, length(symbol));
-        else
-            symbol_idx=ii;
-        end
-        sample_idx=find(ismember(label1, unique_color{i}) & ismember(label2, symbols_now{ii}));
-        h=plot(u(sample_idx,1), u(sample_idx,2), symbol(symbol_idx));
-        set(h,'markersize', 8, 'color', color_map(i,:), 'markerfacecolor', ...
-            color_map(i, :))
-        if i == 1
-            label_legend{count}=symbols_now{ii};
-            count=count+1;
+n = size(u, 2);
+if n == 2
+    figure % 2-D plot
+    hold on
+    set(gcf, 'colormap', parula(256));
+    count=1;
+    for i=1:no_colors
+        label1_now=label1(ismember(label1, unique_color{i}));
+        symbols_now=unique(label2);
+        no_symbols=length(symbols_now);
+        for ii=1:no_symbols
+            if ii>length(symbol)
+                symbol_idx=rem(ii, length(symbol));
+            else
+                symbol_idx=ii;
+            end
+            sample_idx=find(ismember(label1, unique_color{i}) & ismember(label2, symbols_now{ii}));
+            h=plot(u(sample_idx,1), u(sample_idx,2), symbol(symbol_idx));
+            set(h,'markersize', 8, 'color', color_map(i,:), 'markerfacecolor', ...
+                color_map(i, :))
+            if i == 1
+                label_legend{count}=symbols_now{ii};
+                count=count+1;
+            end
         end
     end
+    h = colorbar;
+    set(h, 'Ytick', linspace(0, 1, length(unique_color)), 'YtickLabel', unique_color);
+    legend(label_legend);
+    % h=xlabel('Discriminant function 1'); set(h,'fontsize',14)
+    % h=ylabel('Discriminant function 2'); set(h,'fontisze',14)
+else
+    if n > 3
+        warning('There are more than 3 columns in the data matrix, only first 3 are used to generate the 3-D plot!')
+    end
+    figure % 3-D plot
+    hold on
+    set(gcf, 'colormap', parula(256));
+    count=1;
+    for i=1:no_colors
+        label1_now=label1(ismember(label1, unique_color{i}));
+        symbols_now=unique(label2);
+        no_symbols=length(symbols_now);
+        for ii=1:no_symbols
+            if ii>length(symbol)
+                symbol_idx=rem(ii, length(symbol));
+            else
+                symbol_idx=ii;
+            end
+            sample_idx=find(ismember(label1, unique_color{i}) & ismember(label2, symbols_now{ii}));
+            h=plot3(u(sample_idx,1), u(sample_idx,2), u(sample_idx, 3), symbol(symbol_idx));
+            set(h,'markersize', 8, 'color', color_map(i,:), 'markerfacecolor', ...
+                color_map(i, :))
+            if i == 1
+                label_legend{count}=symbols_now{ii};
+                count=count+1;
+            end
+        end
+    end
+    h = colorbar;
+    set(h, 'Ytick', linspace(0, 1, length(unique_color)), 'YtickLabel', unique_color);
+    legend(label_legend);
+    % h=xlabel('Discriminant function 1'); set(h,'fontsize',14)
+    % h=ylabel('Discriminant function 2'); set(h,'fontisze',14)
 end
-h = colorbar;
-set(h, 'Ytick', linspace(0, 1, length(unique_color)), 'YtickLabel', unique_color);
-legend(label_legend);
-% h=xlabel('Discriminant function 1'); set(h,'fontsize',14)
-% h=ylabel('Discriminant function 2'); set(h,'fontisze',14)
-
                 
